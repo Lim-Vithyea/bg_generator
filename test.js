6d86e760d78988d08f3f107c13340cdf3ac17546
Lim Vithyea <cheakh212@gmail.com>
2024-11-24 18:46:11 +0700

    testing

---
+++ test.js
@@ -0,0 +1,15 @@
+
+let num = 25;
+ if (num==25){
+    console.log("hello ah jm hg bek men??");
+    } else {
+        console.log("ah jmr");
+    }
+for (let i = 1;i<=100;i++){
+    console.log(`${i} ah jm`);
+    while (i == 50){
+        console.log('ah nis lek 50 jg chop ma plet');
+        break;
+    }
+
+}
