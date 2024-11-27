2bdb59073832e581fc62aeb0571a73ce0e3ef1c1
Lim Vithyea <cheakh212@gmail.com>
2024-11-27 10:41:51 +0700

    small changed

--- bggenerator.js
+++ bggenerator.js
@@ -1,17 +1,4 @@
-// var css = document.querySelector("h3");
-// var color1 = document.querySelector("color1");
-// var color2 = document.querySelector("color2");
-// var body = document.getElementById("gradient");
 
-// color1.addEventListener("input", function(){
-//     // body.style.background = 
-//     // "linear-gradient(to right," + color1.value+","+color2.value+");";
-//     console.log(color1.value);
-// })
-
-// color2.addEventListener("input", function(){
-//     console.log(color2.value);
-// })
 
 var css = document.querySelector("h3");
 var color1 = document.getElementById("color1");
@@ -20,9 +7,7 @@ var body = document.getElementById("gradient");
 
 function setGradient() {
     body.style.background = "linear-gradient(to right, " 
-                            + color1.value 
-                            + ", " 
-                            + color2.value 
+                            + color1.value + ", " + color2.value 
                             + ")";
     css.textContent = body.style.background + ";";
 }
